fd4c155d03f81bf4f21cd1394a66b1745a64f4f8
Jessica Fraga <jessica.fraga01@hotmail.com>
2023-10-14 23:44:01 -0300

    Align user img to center; change everything to english

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,26 @@
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 export default function Navbar() {
-	return (
-		<nav className='navbar navbar-dark bg-primary'>
-			<div className='container'>
-				<div className='container-fluid d-flex justify-content-between align-items-center'>
-					<span className='navbar-brand'>
-						<Link to='/'>GitHub Explorer </Link>
-					</span>
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <div className="container">
+        <div className="container-fluid d-flex justify-content-between align-items-center">
+          <span className="navbar-brand">
+            <Link to="/">GitHub Explorer </Link>
+          </span>
 
-					<a
-						href='https://github.com/iKuroiNeko/github-explorer'
-						className='navbar__link'
-						target='_blank'
-						rel='noreferrer'
-					>
-						<i className='bi bi-github'></i>
-						&nbsp;Ver no GitHub!
-					</a>
-				</div>
-			</div>
-		</nav>
-	);
+          <a
+            href="https://github.com/iKuroiNeko/github-explorer"
+            className="navbar__link"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="bi bi-github"></i>
+            &nbsp;Check it out on GitHub!
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,41 +1,41 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router';
-import logo from '../../img/logo.png';
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import logo from "../../img/logo.png";
 
 export default function SearchForm() {
-	const [url, setUrl] = useState(null);
-	const [search, setSearch] = useState('');
+  const [url, setUrl] = useState(null);
+  const [search, setSearch] = useState("");
 
-	let navigate = useNavigate();
+  let navigate = useNavigate();
 
-	const handleSearch = (e) => {
-		e.preventDefault();
-		navigate(`/${search}`);
+  const handleSearch = (e) => {
+    e.preventDefault();
+    navigate(`/${search}`);
 
-		setUrl(`https://api.github.com/users/${search}`);
-	};
-	return (
-		<div>
-			<header>
-				<div className='logo text-center p-4'>
-					<img src={logo} alt='Github Explorer' className='img-fluid' />
-				</div>
-			</header>
-			<div className='search'>
-				<form className='form-inline' onSubmit={handleSearch}>
-					<div className='form-floating'>
-						<input
-							type='search'
-							className='form-control sr-only'
-							id='floatingInput'
-							placeholder='Pesquisar usuário'
-							onChange={(e) => setSearch(e.target.value)}
-						/>
-						<label htmlFor='floatingInput'>Pesquisar usuário</label>
-					</div>
-					<button className=' btn btn-primary'>Pesquisar</button>
-				</form>
-			</div>
-		</div>
-	);
+    setUrl(`https://api.github.com/users/${search}`);
+  };
+  return (
+    <div>
+      <header>
+        <div className="logo text-center p-4">
+          <img src={logo} alt="Github Explorer" className="img-fluid" />
+        </div>
+      </header>
+      <div className="search">
+        <form className="form-inline" onSubmit={handleSearch}>
+          <div className="form-floating">
+            <input
+              type="search"
+              className="form-control sr-only"
+              id="floatingInput"
+              placeholder="Pesquisar usuário"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <label htmlFor="floatingInput">Search user</label>
+          </div>
+          <button className=" btn btn-primary">Search</button>
+        </form>
+      </div>
+    </div>
+  );
 }
